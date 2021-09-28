8bf5ae57f5c87475eac33f04b42362559580fa39
ninodl <n.diaz10@ufromail.cl>
2021-09-27 21:36:48 -0300

    Agregado clase HolaMundo2.java

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Javier");
-        System.out.println("Hola Ninoska");
+        System.out.println("Hola Mundo me llamo nino");
     }
 }
